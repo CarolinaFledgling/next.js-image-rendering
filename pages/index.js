d0f9c217aa0b43ccd8f31b94947866220a52838b
Karolina Kulinska <karolina.kulinska89@gmail.com>
2022-05-30 21:13:07 +0200

    learning different ways to render images in next.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import bikeImg from "../images/bike.jpg";
+// static  img import
 
 export default function Home() {
   return (
@@ -11,59 +13,60 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+      <div>
+        <p>Very basic image source tag, inspect it in network </p>
+        <img
+          height="500"
+          src="https://cdn.pixabay.com/photo/2016/01/09/18/27/camera-1130731_1280.jpg"
+        />
+      </div>
+      <div>
+        <p>Using Image component from next.js with a static img import</p>
+        <p>
+          When we have a local img when it optimizes and builds, its essentially
+          going to know what the height and width is because we have access to
+          the file
         </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <Image src={bikeImg} alt="bike" />
+      </div>
+      <div>
+        <p>Using Image component from next.js with a remote img from url</p>
+        <p>
+          we need to provide url and we need to tell next.js the height and
+          width of img{" "}
+        </p>
+        <p>
+          we need to provide url and we need to tell next.js the height and
+          width of img, next.js doesnt know height and width, it doesnt know how
+          to handle that file from url
+        </p>
+        <Image
+          src="https://cdn.pixabay.com/photo/2016/01/09/18/27/camera-1130731_1280.jpg"
+          alt="bike"
+          height={1080}
+          width={1920}
+        />
+      </div>
+      <div>
+        <p>Using Image component from next.js with a remote img from url</p>
+        <p>
+          but if we dont know what the height and width is, and we want to have
+          more responsive dynamic approach to styling, instead of height and
+          with in Image component we can set layout="fill"
+        </p>
+        <p>
+          what this will do is it will essentially become the size of the parent
+          component
+        </p>
+        <div className="imgBox">
+          <Image
+            src="https://cdn.pixabay.com/photo/2016/01/09/18/27/camera-1130731_1280.jpg"
+            alt="bike"
+            layout="fill"
+            objectFit="contain"
+          />
         </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+      </div>
     </div>
-  )
+  );
 }
